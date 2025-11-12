842ebf625aa62addf873509a7ca8522fe5590c90
Andreea Buzenchi <138552655+ABuzenchi@users.noreply.github.com>
2025-11-13 00:15:32 +0200

    Add Statistics Chart

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -2,12 +2,13 @@ import { CourseCarousel } from "../../components/courseCarusel/courseCarusel";
 import { CourseList } from "../../components/courseList/courseList";
 import { DashboardHeader } from "../../components/DashboardHeader/DashboardHeader";
 import { NavbarComponent } from "../../components/navbar/navbar"
+import { StatisticsChart } from "../../components/StatisticsChart/StatisticsChart";
 import { WelcomeCard } from "../../components/welcomeCard/welcomeCard";
 
 const User=()=>{
     return(
     <div>
-        <DashboardHeader/>
+        <StatisticsChart/>
     </div>
     )
 }
