7e50a6629728408e671ca9cb3e9403524d0290d7
Andreea Buzenchi <138552655+ABuzenchi@users.noreply.github.com>
2025-11-12 23:53:57 +0200

    Add course carusel

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,10 +1,11 @@
+import { CourseCarousel } from "../../components/courseCarusel/courseCarusel";
 import { NavbarComponent } from "../../components/navbar/navbar"
 import { WelcomeCard } from "../../components/welcomeCard/welcomeCard";
 
 const User=()=>{
     return(
     <div>
-        <WelcomeCard/>
+        <CourseCarousel/>
     </div>
     )
 }
